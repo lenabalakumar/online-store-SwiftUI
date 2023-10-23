28684108195dd020e5846bdf47d052a64b78f73a
Lenabalakumar Subbarayan <lenabalakumar@gmail.com>
2023-10-23 11:53:25 +0100

    Initial Commit

---
+++ online-store/online_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  online_storeApp.swift
+//  online-store
+//
+//  Created by Lenabalakumar Subbarayan on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct online_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
